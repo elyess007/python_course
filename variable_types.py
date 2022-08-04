bc07ce5db519f79f66ce69e232b9accdc3d0397e
elyeselhakim <elyes.elhakim2007@gmail.com>
2022-08-04 15:15:43 +0100

    variable types

---
+++ variable_types.py
@@ -0,0 +1,14 @@
+# EXECUTE PYTHON SCRIPT : python scriptName.py
+
+# TEXT 
+x0 = int(1)
+x1 = str("\nBonjour, je m'appelle elyes !") # \n : retour a la ligne
+print(str(x0) + x1)
+
+x2 = float(3.14)
+print("Ceci est un float : "+str(x2))
+
+mois = ["janvier", "fevrier", "mars"]
+print(str(mois))
+print(str(mois[0]))
+
